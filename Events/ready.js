d715820fc6cfce0350ce541e62e0b966e2020c9a
Arkyan <didryjules@gmail.com>
2024-09-03 19:42:53 +0200

    ça marche

--- Events/ready.js
+++ Events/ready.js
@@ -7,4 +7,15 @@ module.exports = async bot  => {
     await loadSlashCommands(bot);
     console.log(`Connecté en tant que ${bot.user.tag} !`);
 
+    bot.on("messageCreate", message => {
+    // Vérifie si le message vient du canal spécifié et ignore les messages du bot
+    if (message.channel.id === "871370018040987709" && !message.author.bot) {
+        console.log(`Nouveau message de ${message.author.username} dans le canal ${message.channel.name}`);
+        // Exemple : réponse automatique
+        var channel = bot.channels.cache.get("1280502721958383646");
+        channel.send(`Annonce pris en compte dans le salon : ${message.channel.name} avec ID = ${message.channel.id}`);
+    }
+});
+
 }
+
